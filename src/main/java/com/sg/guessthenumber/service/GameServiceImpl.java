d3be7d9f00fd86158dfba0625f0ba9b85ad5ca05
salmakhairat <79557473+salmakhairat@users.noreply.github.com>
2023-07-12 16:37:38 -0400

    Update GameServiceImpl.java

--- src/main/java/com/sg/guessthenumber/service/GameServiceImpl.java
+++ src/main/java/com/sg/guessthenumber/service/GameServiceImpl.java
@@ -10,44 +10,82 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class GameServiceImpl implements GameService {
-
-    GameDao gameDao;
-
-    RoundDao roundDao;
-
-    @Autowired
-    public GameServiceImpl(GameDao gameDao, RoundDao roundDao) {
         this.gameDao = gameDao;
         this.roundDao = roundDao;
     }
-
     @Override
     public String generateAnswer() {
-        return null;
+        Random random = new Random();
+        String number = "";
+        for (int i = 0; i < 4; i++) {
+            number += String.valueOf(random.nextInt(10));
+        }
+        return number;
     }
 
     @Override
     public int beginGame() {
-        return 0;
+        String answer = generateAnswer();
+        Game game = new Game(answer, false);
+        game = gameDao.add(game);
+        return game.getId();
     }
 
     @Override
     public Round guessNumber(Round round) {
-        return null;
+        Game game = gameDao.findById(round.getGameId());
+        String answer = game.getAnswer();
+        String guess = round.getGuess();
+
+        if (game.isFinished()) {
+            throw new RuntimeException("Cannot place guess for a finished game.");
+        }
+
+        int e = 0;
+        int p = 0;
+
+        for (int i = 0; i < 4; i++) {
+            char answerChar = answer.charAt(i);
+            char guessChar = guess.charAt(i);
+
+            if (answer.contains(String.valueOf(guessChar))) {
+                if (answerChar == guessChar) {
+                    e++;
+                } else {
+                    p++;
+                }
+            }
+        }
+        round.setResult("e:" + e + ":p:" + p);
+        roundDao.add(round);
+
+        if (e == 4) {
+            game.setFinished(true);
+            gameDao.update(game);
+        }
+        return round;
     }
 
     @Override
     public List<Game> getAllGames() {
-        return null;
+        return gameDao.getAll();
     }
 
     @Override
     public Game getGameById(int gameId) {
-        return null;
+        Game game = gameDao.findById(gameId);
+        if (game == null) {
+            throw new RuntimeException("Game ID does not exist.");
+        }
+        return game;
     }
 
     @Override
     public List<Round> getRoundsByGameId(int gameId) {
-        return null;
+        List<Round> rounds = roundDao.findByInt(gameId);
+        if (rounds == null) {
+            throw new RuntimeException("Game ID does not exist.");
+        }
+        return rounds;
     }
 }
