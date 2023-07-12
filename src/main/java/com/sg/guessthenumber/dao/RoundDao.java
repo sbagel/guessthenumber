88de660db7b1e0a58df03d3b58cbe6d5ba6c2fde
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-07-12 15:30:51 -0400

    added DAO interfaces for Game and Round

---
+++ src/main/java/com/sg/guessthenumber/dao/RoundDao.java
@@ -0,0 +1,10 @@
+package com.sg.guessthenumber.dao;
+
+import com.sg.guessthenumber.dto.Round;
+
+import java.util.List;
+
+public interface RoundDao {
+    int createRound(Round round);
+    List<Round> getRoundsByGameId(int gameId);
+}
