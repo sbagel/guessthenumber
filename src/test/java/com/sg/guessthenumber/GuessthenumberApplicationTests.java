e13f43c239c35bad5ab5fd6eecf6c5adf33edf99
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-12 10:32:43 -0700

    first commit

---
+++ src/test/java/com/sg/guessthenumber/GuessthenumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.guessthenumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessthenumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
