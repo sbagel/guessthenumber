7bd9676bd22ab3ee889e9e8ca610ae933a8a9af3
strangechick <fkhondker123@gmail.com>
2023-07-12 15:35:12 -0400

    GAME DAO IMPL and ROUNDDAO IMPL

--- src/main/java/com/sg/guessthenumber/dao/GameDAOImpl.java
+++ src/main/java/com/sg/guessthenumber/dao/GameDAOImpl.java
@@ -1,2 +1,67 @@
-package com.sg.guessthenumber.dao;public class GameDAOImpl {
+package com.sg.guessthenumber.dao;
+
+import com.sg.guessthenumber.dto.Game;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+public class GameDAOImpl implements GameDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public GameDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int createGame(Game game) {
+        String sql = "INSERT INTO game (answer, finished) VALUES (?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, game.getAnswer());
+            ps.setBoolean(2, game.isFinished());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().intValue();
+    }
+
+    @Override
+    public Game getGameById(int gameId) {
+        String sql = "SELECT * FROM game WHERE game_id = ?";
+        return jdbcTemplate.queryForObject(sql, new GameRowMapper(), gameId);
+    }
+
+    @Override
+    public List<Game> getAllGames() {
+        String sql = "SELECT * FROM game";
+        return jdbcTemplate.query(sql, new GameRowMapper());
+    }
+
+    @Override
+    public void updateGame(Game game) {
+        String sql = "UPDATE game SET answer = ?, finished = ? WHERE game_id = ?";
+        jdbcTemplate.update(sql, game.getAnswer(), game.isFinished(), game.getGameId());
+    }
+
+    private static class GameRowMapper implements RowMapper<Game> {
+        @Override
+        public Game mapRow(ResultSet rs, int rowNum) throws SQLException {
+            int gameId = rs.getInt("game_id");
+            String answer = rs.getString("answer");
+            boolean finished = rs.getBoolean("finished");
+            return new Game(gameId, answer, finished);
+        }
+    }
 }
--- src/main/java/com/sg/guessthenumber/dao/RoundDAOImpl.java
+++ src/main/java/com/sg/guessthenumber/dao/RoundDAOImpl.java
@@ -1,2 +1,60 @@
-package com.sg.guessthenumber.dao;public class RoundDAOImpl {
-}
+package com.sg.guessthenumber.dao;
+
+import com.sg.guessthenumber.dto.Round;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+    @Repository
+    public class RoundDAOImpl implements RoundDAO {
+
+        private final JdbcTemplate jdbcTemplate;
+
+        public RoundDAOImpl(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+        @Override
+        public int createRound(Round round) {
+            String sql = "INSERT INTO round (game_id, guess, guess_time, result) VALUES (?, ?, ?, ?)";
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setInt(1, round.getGameId());
+                ps.setString(2, round.getGuess());
+                ps.setTimestamp(3, Timestamp.valueOf(round.getGuessTime()));
+                ps.setString(4, round.getResult());
+                return ps;
+            }, keyHolder);
+
+            return keyHolder.getKey().intValue();
+        }
+
+        @Override
+        public List<Round> getRoundsByGameId(int gameId) {
+            String sql = "SELECT * FROM round WHERE game_id = ?";
+            return jdbcTemplate.query(sql, new RoundRowMapper(), gameId);
+        }
+
+        private static class RoundRowMapper implements RowMapper<Round> {
+            @Override
+            public Round mapRow(ResultSet rs, int rowNum) throws SQLException {
+                int roundId = rs.getInt("round_id");
+                int gameId = rs.getInt("game_id");
+                String guess = rs.getString("guess");
+                LocalDateTime guessTime = rs.getTimestamp("guess_time").toLocalDateTime();
+                String result = rs.getString("result");
+                return new Round(roundId, gameId, guess, guessTime, result);
+            }
+        }
+    }
+
+
