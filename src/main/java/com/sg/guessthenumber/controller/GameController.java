22914d12affb5e77bf69b239311d0e426a6bdea6
Melissa Augustin <melissaaugustin1998@gmail.com>
2023-07-13 10:37:37 -0400

    added controller

---
+++ src/main/java/com/sg/guessthenumber/controller/GameController.java
@@ -0,0 +1,44 @@
+package com.sg.guessthenumber.controller;
+
+import com.sg.guessthenumber.dto.Game;
+import com.sg.guessthenumber.dto.Round;
+import com.sg.guessthenumber.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/api/game")
+public class GameController {
+    private final GameService gameService;
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+    @PostMapping("/begin")
+    public ResponseEntity<Integer> beginGame() {
+        int gameId = gameService.beginGame();
+        return ResponseEntity.status(HttpStatus.CREATED).body(gameId);
+    }
+    @PostMapping("/guess")
+    public ResponseEntity<Round> makeGuess(@RequestBody Round roundDTO) {
+        Round result = gameService.guess(roundDTO.getGameId(), roundDTO.getGuess());
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<Game>> getAllGames() {
+        List<Game> games = gameService.getAllGames();
+        return ResponseEntity.status(HttpStatus.OK).body(games);
+    }
+    @GetMapping("/{gameId}")
+    public ResponseEntity<Game> getGameById(@PathVariable int gameId) {
+        Game game = gameService.getGameById(gameId);
+        return ResponseEntity.status(HttpStatus.OK).body(game);
+    }
+    @GetMapping("/rounds/{gameId}")
+    public ResponseEntity<List<Round>> getRoundsByGameId(@PathVariable int gameId) {
+        List<Round> rounds = gameService.getRoundsByGameId(gameId);
+        return ResponseEntity.status(HttpStatus.OK).body(rounds);
+    }
+}
