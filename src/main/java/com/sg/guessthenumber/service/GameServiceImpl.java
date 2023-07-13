89f57b16aebd0c3e39f64e252b24b1ab27495941
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-07-13 13:41:06 -0400

    added pseudo code in GameServiceImpl

--- src/main/java/com/sg/guessthenumber/service/GameServiceImpl.java
+++ src/main/java/com/sg/guessthenumber/service/GameServiceImpl.java
@@ -27,42 +27,58 @@ public class GameServiceImpl implements GameService {
         Random random = new Random();
         String number = "";
         HashSet<Integer> set = new HashSet<>();
-        while(true) {
+
+        while (true) {
+            // Generate a random number between 0 and 9
             int randomNum = random.nextInt(10);
+            // Check if the number is not already in the set
             if (!set.contains(randomNum)) {
+                // Add the number to the set
                 set.add(randomNum);
+                // Append the number to the string
                 number += String.valueOf(randomNum);
             }
-            if (set.size() == 4) break;
+            // Check if we have generated four unique numbers
+            if (set.size() == 4)
+                break;
         }
+
         return number;
     }
 
     @Override
     public int beginGame() {
+        // Generate a random answer using the generateAnswer() method
         String answer = generateAnswer();
+        // Create a new Game object with the generated answer and set it as unfinished
         Game game = new Game(answer, false);
+        // Save the game object to the data access object and return the generated game ID
         return gameDao.createGame(game);
     }
 
+
     @Override
     public Round guessNumber(Round round) {
+        // Get the game object based on the game ID in the round
         Game game = gameDao.getGameById(round.getGameId());
         String answer = game.getAnswer();
         String guess = round.getGuess();
-
+        // Check if the game is already finished
         if (game.isFinished()) {
             throw new RuntimeException("Cannot place guess for a finished game.");
         }
 
         int e = 0;
         int p = 0;
-
+        // Iterate over each character position in the strings (assuming the strings are always of length 4)
         for (int i = 0; i < 4; i++) {
+            // Get the character at the i-th position in the answer string
             char answerChar = answer.charAt(i);
+            // Get the character at the i-th position in the guess string
             char guessChar = guess.charAt(i);
-
+            // Check if the guess character is present in the answer string
             if (answer.contains(String.valueOf(guessChar))) {
+                // Check if the characters at the same position match exactly
                 if (answerChar == guessChar) {
                     e++;
                 } else {
@@ -70,25 +86,33 @@ public class GameServiceImpl implements GameService {
                 }
             }
         }
+        // Set the result of the round as a formatted string combining the exact and partial match counts
         round.setResult("e:" + e + ":p:" + p);
+        // Save the round object to the database
         roundDao.createRound(round);
-
+        // Check if all characters are exact matches
         if (e == 4) {
+            // Set the game as finished
             game.setFinished(true);
+            // Update the game object in the database
             gameDao.updateGame(game);
         }
+
         return round;
     }
 
+
     @Override
+    //Retrieve all games from the data access object and return the list
     public List<Game> getAllGames() {
         return gameDao.getAllGames();
     }
 
     @Override
     public Game getGameById(int gameId) {
+        // Retrieve the game by its ID from the data access object
         Game game = gameDao.getGameById(gameId);
-        if (game == null) {
+        if (game == null) {  // Check if the game object is null
             throw new RuntimeException("Game ID does not exist.");
         }
         return game;
@@ -96,10 +120,12 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public List<Round> getRoundsByGameId(int gameId) {
+        // Retrieve the rounds associated with the given game ID from the data access object
         List<Round> rounds = roundDao.getRoundsByGameId(gameId);
         if (rounds == null) {
             throw new RuntimeException("Game ID does not exist.");
         }
         return rounds;
     }
+
 }
---
+++ target/classes/schema.sql
@@ -0,0 +1,34 @@
+-- DROP DATABASE IF EXISTS guessthenumberdb;
+
+-- create database guessthenumberdb;
+-- use guessthenumberdb;
+
+-- Table structure for game
+
+DROP TABLE IF EXISTS game;
+CREATE TABLE game (
+  game_id INT PRIMARY KEY AUTO_INCREMENT,
+  answer VARCHAR(4) NOT NULL,
+  finished BOOLEAN DEFAULT false
+);
+
+-- Table structure for round
+
+DROP TABLE IF EXISTS round;
+CREATE TABLE round (
+  round_id INT PRIMARY KEY AUTO_INCREMENT,
+  game_id INT NOT NULL,
+  guess VARCHAR(4) NOT NULL,
+  guess_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  result VARCHAR(7) NOT NULL,
+  FOREIGN KEY (game_id) REFERENCES Game(game_id)
+);
+
+INSERT INTO game (answer, finished) VALUES
+	('5213', true),
+	('7168', false);
+
+INSERT INTO round (game_id, guess_time, guess, result) VALUES
+    (1, '2023-07-01 07:25:18', '1234', 'e:1:p:2'),
+    (1, '2023-07-01 09:11:07', '3215', 'e:2:p:2'),
+    (1, '2023-07-01 09:11:49', '5213', 'e:4:p:0');
